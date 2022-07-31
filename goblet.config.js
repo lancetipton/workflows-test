6b99f9548bd7611f8a43980c72a0ab06a2392f6d
lancetipton <lancetipton@goblet.io>
2022-07-31 01:15:53 +0000

    Update 18 items. Added 18 items.

---
+++ goblet.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  recorder: {
+    locator: `-- 🍷 GOBLET`,
+  },
+  paths: {
+    repoRoot: __dirname,
+    workDir: 'goblet',
+    artifactsDir: 'artifacts',
+    reportsDir: 'artifacts/reports',
+    featuresDir: 'bdd/features',
+    supportDir: 'bdd/support',
+    world: 'bdd/support/world.js',
+    stepsDir: 'bdd/steps',
+    unitDir: 'unit',
+    waypointDir: 'waypoint',
+  },
+}
